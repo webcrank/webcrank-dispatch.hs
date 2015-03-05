a51c455c8df43d03528ce445f511b8c26951f5af
Richard Wallace <rwallace@thewallacepack.net>
2015-03-05 14:58:17 -0700

    some cleanup

--- src/Webcrank/Dispatch.hs
+++ src/Webcrank/Dispatch.hs
@@ -14,6 +14,7 @@ module Webcrank.Dispatch
   , renderRoute
   , renderRoute'
   , (.*.)
+  , HVect(..)
   ) where
 
 import Control.Monad.Identity
--- tests/doctests.hs
+++
@@ -1,9 +0,0 @@
-
-import Test.DocTest
-main = doctest
-  [ "-isrc"
-  , "src/Web/Crank/Dispatch.hs"
-  , "src/Web/Crank/Dispatch/Path.hs"
-  , "src/Web/Crank/Dispatch/Route.hs"
-  , "src/Web/Crank/Dispatch/Types.hs"
-  ]
